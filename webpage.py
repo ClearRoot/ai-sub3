35d1dd78ed079bcf2c22caf7f1d258a5846dbdc1
Lee Jiseon <wiahawm7@gmail.com>
2019-09-19 17:44:50 +0900

    S1P3143001-23 chat box 구현

--- webpage.py
+++ webpage.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
  
 app = Flask(__name__)
 
-@app.route('/')
+comments = []
+@app.route('/', methods = ['POST', 'GET'])
 def index():
-	return render_template('index.html')
+	if request.method == 'POST':
+		temp = request.form
+		for i in temp.values():
+			comments.append(i)
+		return render_template('index.html', comments=comments)
+	elif request.method == 'GET':
+		temp = request.args.get('comment')
+		return render_template('index.html', comment=temp)
 
 @app.route('/about')
 def about():
