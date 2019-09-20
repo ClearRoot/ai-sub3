b9ee44c5a47a888fbffba5a435890f993cdd215e
Lee Jiseon <wiahawm7@gmail.com>
2019-09-20 14:27:24 +0900

    background image 변경, header 변경, datetime 적용

--- webpage.py
+++ webpage.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
- 
+import datetime
+
 app = Flask(__name__)
 
 comments = []
 @app.route('/', methods = ['POST', 'GET'])
 def index():
+	now = datetime.datetime.now()
 	if request.method == 'POST':
+		time= now.strftime('%H:%M:%S')
 		temp = request.form
 		for i in temp.values():
 			comments.append(i)
-		return render_template('index.html', comments=comments)
+		return render_template('index.html', comments=comments, time=time)
 	elif request.method == 'GET':
 		temp = request.args.get('comment')
 		return render_template('index.html', comment=temp)
