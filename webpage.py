bce7c822536688e91b1831418dd45212aece32e8
KimHoon <gnsl8391@gmail.com>
2019-09-23 16:28:57 +0900

    webpage.py 와 templates 수정 chatbot 구현중
    Former-commit-id: 9439b9b039cf4c3a0322f79bb1f4f7954df11651

--- webpage.py
+++ webpage.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import datetime
  
 app = Flask(__name__)
 
-@app.route('/')
+comments = []
+
+@app.route('/', methods = ['POST', 'GET'])
 def index():
-	return render_template('index.html')
+   now = datetime.datetime.now()
+   if request.method == 'POST':
+      time = now.strftime('%H:%M:%S')
+      temp = request.form
+      for i in temp.values():
+         comments.append(i)
+      return render_template('index.html', comments=comments, time=time)
+   elif request.method == 'GET':
+      temp = request.args.get('comment')
+      return render_template('index.html', comment=temp)
 
 @app.route('/about')
 def about():
